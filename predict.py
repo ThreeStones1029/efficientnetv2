df47dc7505d6d3805111eb39ce59940c56850ecf
ThreeStones1029 <2320218115@qq.com>
2024-04-01 13:31:48 +0000

    first commit

---
+++ predict.py
@@ -0,0 +1,65 @@
+'''
+Description: 
+version: 
+Author: ThreeStones1029 2320218115@qq.com
+Date: 2024-03-31 04:04:02
+LastEditors: ShuaiLei
+LastEditTime: 2024-04-01 12:18:04
+'''
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import efficientnetv2_s as create_model
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    img_size = {"s": [300, 384],  # train_size, val_size
+                "m": [384, 480],
+                "l": [384, 480]}
+    num_model = "s"
+    data_transform = transforms.Compose(
+        [transforms.Resize(img_size[num_model][1]),
+         transforms.CenterCrop(img_size[num_model][1]),
+         transforms.ToTensor(),
+         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    # load image
+    img_path = "dataset/spine_fracture/fracture_images/sub-verse113_LA_L5_2.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+    # create model
+    model = create_model(num_classes=2).to(device)
+    # load model weights
+    model_weight_path = "./weights/val_best_model.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)], predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)], predict[i].numpy()))
+    # plt.show()
+    plt.savefig("predict.png")
+
+
+if __name__ == '__main__':
+    main()
